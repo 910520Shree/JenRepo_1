fa3201f7e3dbb44a33112e4328602c57ee885c1a
910520Shree <shwethashreebh@gmail.com>
2023-12-15 17:20:37 +0530

    1st push

---
+++ JenProject_1/src/test/java/jenTest/JenDemoTest.java
@@ -0,0 +1,19 @@
+package jenTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class JenDemoTest {
+	
+	@Test
+	public void m1Test()
+	{
+		WebDriverManager.edgedriver().setup();
+		WebDriver driver=new EdgeDriver();
+		driver.get("https://www.facebook.com");
+	}
+
+}
